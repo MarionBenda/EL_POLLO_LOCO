78fc5a865cf8e3240c200a0ec50e103305d002d0
MarionBenda <marionbenda@yahoo.de>
2026-06-10 15:17:05 +0200

    Refactor BackgroundObject and MovableObject classes; add animation and image loading functionality to Character and Chicken classes

--- models/character.class.js
+++ models/character.class.js
@@ -3,8 +3,29 @@ class Character extends MovableObject {
   width = 100;
   y = 155;
 
+  IMAGES_WALKING = [
+    'img/2_character_pepe/2_walk/W-21.png',
+    'img/2_character_pepe/2_walk/W-22.png',
+    'img/2_character_pepe/2_walk/W-23.png',
+    'img/2_character_pepe/2_walk/W-24.png',
+    'img/2_character_pepe/2_walk/W-25.png',
+    'img/2_character_pepe/2_walk/W-26.png',
+  ];
+
   constructor() {
     super().loadImage('img/2_character_pepe/2_walk/W-21.png');
+    this.loadImages(this.IMAGES_WALKING);
+
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      let index = this.currentImage % this.IMAGES_WALKING.length;
+      let path = this.IMAGES_WALKING[index];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 100);
   }
 
   jump() {}
