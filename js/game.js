bec16a90be584d6026e6b16b0bd8828c7c5cfe35
MarionBenda <marionbenda@yahoo.de>
2026-06-11 19:05:26 +0200

    Add keyboard and level management; refactor world and character classes for improved functionality

--- js/game.js
+++ js/game.js
@@ -1,11 +1,52 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init() {
   canvas = document.getElementById('gameCanvas');
-  world = new World(canvas);
+  world = new World(canvas, keyboard);
 
-  console.log('My Character is', world.myCharacter);
+  console.log('My Character is', world.character);
   console.log('Enemies are', world.enemies);
   console.log('World is', world);
 }
+
+window.addEventListener('keydown', (e) => {
+  if (e.code === 'ArrowRight') {
+    keyboard.RIGHT = true;
+  }
+  if (e.code === 'ArrowLeft') {
+    keyboard.LEFT = true;
+  }
+  if (e.code === 'Space') {
+    keyboard.SPACE = true;
+  }
+
+  if (e.code === 'up') {
+    keyboard.UP = true;
+  }
+
+  if (e.code === 'down') {
+    keyboard.DOWN = true;
+  }
+});
+
+window.addEventListener('keyup', (e) => {
+  if (e.code === 'ArrowRight') {
+    keyboard.RIGHT = false;
+  }
+  if (e.code === 'ArrowLeft') {
+    keyboard.LEFT = false;
+  }
+  if (e.code === 'Space') {
+    keyboard.SPACE = false;
+  }
+
+  if (e.code === 'up') {
+    keyboard.UP = false;
+  }
+
+  if (e.code === 'down') {
+    keyboard.DOWN = false;
+  }
+});
--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
   height = 280;
   width = 100;
   y = 155;
+  speed = 10;
 
   IMAGES_WALKING = [
     'img/2_character_pepe/2_walk/W-21.png',
@@ -12,6 +13,8 @@ class Character extends MovableObject {
     'img/2_character_pepe/2_walk/W-26.png',
   ];
 
+  world;
+
   constructor() {
     super().loadImage('img/2_character_pepe/2_walk/W-21.png');
     this.loadImages(this.IMAGES_WALKING);
@@ -21,11 +24,26 @@ class Character extends MovableObject {
 
   animate() {
     setInterval(() => {
-      let index = this.currentImage % this.IMAGES_WALKING.length;
-      let path = this.IMAGES_WALKING[index];
-      this.img = this.imageCache[path];
-      this.currentImage++;
-    }, 100);
+      if (this.world.keyboard.RIGHT) {
+        this.x += this.speed;
+        this.otherDirection = false;
+      }
+
+      if (this.world.keyboard.LEFT) {
+        this.x -= this.speed;
+        this.otherDirection = true;
+      }
+      this.world.camera_x = -this.x;
+    }, 1000 / 60);
+
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+        let index = this.currentImage % this.IMAGES_WALKING.length;
+        let path = this.IMAGES_WALKING[index];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+      }
+    }, 50);
   }
 
   jump() {}
